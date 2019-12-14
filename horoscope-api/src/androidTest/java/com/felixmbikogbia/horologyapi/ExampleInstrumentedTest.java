c413684781189b78443fb4be886eb99facaadede
Felix Mbikogbia <mbikogbiafelix@gmail.com>
2019-12-14 16:51:27 -0600

    Initial commit

---
+++ horoscope-api/src/androidTest/java/com/felixmbikogbia/horologyapi/ExampleInstrumentedTest.java
@@ -0,0 +1,31 @@
+package com.felixmbikogbia.horologyapi;
+
+import android.content.Context;
+
+import static junit.framework.Assert.assertEquals;
+
+//import android.support.test.InstrumentationRegistry;
+//import android.support.test.runner.AndroidJUnit4;
+//import static org.junit.Assert.*;
+//
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("bot.box.horologyapi.test", appContext.getPackageName());
+    }
+}
+
+
